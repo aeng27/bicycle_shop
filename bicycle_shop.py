9c349a1a0d992aad571f0970d789420f8a0a5e5a
Alyssa Eng <ae7379@gmail.com>
2015-05-25 23:05:40 +0000

    Initial Commit

---
+++ bicycle_shop.py
@@ -0,0 +1,8 @@
+class bicycle(object):
+  def __init__(self, name, weight, production_cost):
+      self.name = name
+      self.weight = weight
+      self.production_cost = production_cost
+
+
+      
